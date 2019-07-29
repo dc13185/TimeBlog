cb47464b1b35c28eeaf68264b70908baa1c6eb75
dc13185 <1318533144@qq.com>
2019-07-29 19:32:57 +0800

    新增实体类

--- time-core/src/main/java/com/timeblog/business/domain/Label.java
+++ time-core/src/main/java/com/timeblog/business/domain/Label.java
@@ -1,11 +1,15 @@
 package com.timeblog.business.domain;
 
+import lombok.Data;
+
 /**
  * @author: dong.chao
  * @create: 2019-07-29 18:36
  * @description: 文章标签
  **/
+@Data
 public class Label {
+
     private int labelId;
 
     private String labelName;
