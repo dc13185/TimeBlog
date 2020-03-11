69a59d443f57f05a9629900c202ed11c71132af8
clw2404431 <2404431925@qq.com>
2020-03-11 14:55:09 +0800

    后台评论回复功能，有些问题

--- time-admin/src/main/java/com/timeblog/admin/controller/comment/CommentsController.java
+++ time-admin/src/main/java/com/timeblog/admin/controller/comment/CommentsController.java
@@ -1,9 +1,16 @@
 package com.timeblog.admin.controller.comment;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
 import com.timeblog.business.base.Result;
 import com.timeblog.business.domain.*;
 import com.timeblog.framework.mapper.*;
+import com.timeblog.framework.system.constant.InterfaceConstant;
+import com.timeblog.framework.system.constant.SystemConstant;
+import com.timeblog.framework.system.utils.HttpClientUtils;
 import com.timeblog.framework.system.utils.RedisUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,38 +21,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
+
 /**
- * @author DongChao
- * @Classname ArticleController
+ * @author clw
+ * @Classname CommentsController
  * @Description 文章业务
- * @Date 2020/2/9 10:27
+ * @Date 2020/3/9 10:27
  * @Version V1.0
  */
 @Controller
 @RequestMapping("/comment")
 public class CommentsController {
 
-    private final static Logger logger =LoggerFactory.getLogger(CommentsController.class);
 
-    @Value("${imageFile.path}")
-    private String imageFilePath;
-
-    @Value("${imageFile.url}")
-    private String imageUrl;
 
     @Autowired
     private  RedisUtils redisUtils;
 
     @Resource
     private CommentDao commentDao;
-
-    @Resource
-    private ArticleTypeMapper articleTypeMapper;
-
-    @Resource
-    private LabelMapper labelMapper;
 
     @Resource
     private ArticleLabelMapper articleLabelMapper;
@@ -60,7 +62,7 @@ public class CommentsController {
 
     @RequestMapping("/showCommentList")
     @ResponseBody
-    public Result showCommentListWithPage(@RequestBody PageDomain pageDomain){
+    public Result showCommentListWithPage(@RequestBody PageDomain pageDomain) throws ParseException {
         List<Comment> commentList =  commentDao.queryAll(pageDomain);
         return Result.success(commentList);
     }
@@ -71,8 +73,76 @@ public class CommentsController {
     public Result delete(@RequestBody HashMap<String,String> map){
         String commentIds = map.get("commentIds");
         List<String> commentIdsList = Arrays.asList(commentIds.split(","));
+
         commentDao.deleteByIds(commentIdsList);
         return Result.success();
+    }
+    @RequestMapping("/commentRepaly")
+    @ResponseBody
+    public Result commentRepaly(@RequestBody HashMap<String,String> map){
+        String replayId = map.get("replayId");
+        String picture=null;
+        String nickName=null;
+        String commentContent=map.get("commentContent");
+        String commentQQ=SystemConstant.BLOGWEBCONFIG.getBlogAuthorQq();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date=new Date(System.currentTimeMillis());
+        Comment comment=new Comment();
+        if (StringUtils.isNotEmpty(commentQQ)){
+            //QQ不为空获取其头像，昵称
+            Map<String,String> infoMap = null;
+            try {
+                infoMap = getPictureAndNickNameByQq(commentQQ);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            picture = infoMap.get("picture");
+            nickName = infoMap.get("nickName");
+        }
+        String adress=null;
+        String ip=null;
+        try {
+             ip= InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        if (StringUtils.isBlank(nickName)){
+            nickName = "匿名";
+        }
+        comment.setCommentAddress(adress);
+        comment.setCommentIp(ip);
+        comment.setCommentQQ(commentQQ);
+        comment.setParentCommentId(Integer.parseInt(replayId));
+        comment.setCommentContent(commentContent);
+        comment.setCreateTime(simpleDateFormat.format(date));
+        comment.setCommentNickname(nickName);
+        comment.setCommentPicture(picture);
+        commentDao.insert(comment);
+        return Result.success();
+    }
+
+
+    private Map<String,String> getPictureAndNickNameByQq(String Qq) throws IOException {
+        String nickName = null;
+        String picture = String.format(InterfaceConstant.GET_QQ_PICTURE_INTERFACE,Qq);
+        String getNickNameUrl = String.format(InterfaceConstant.GET_QQ_NICKNAME_INTERFACE,Qq);
+        String  resultStr =  HttpClientUtils.getRequest(getNickNameUrl);
+        if (StringUtils.isNotEmpty(resultStr)){
+            String jsonContent = StringUtils.substringBetween(resultStr,"portraitCallBack(",")");
+            JSONObject jsonObject = JSONObject.parseObject(jsonContent);
+            JSONArray jsonArray =  jsonObject.getJSONArray(Qq);
+            String nikNameStr =  jsonArray.getString(6);
+            if (StringUtils.isNotBlank(nikNameStr)){
+                //获取昵称
+                nickName = nikNameStr;
+            }
+        }
+
+        Map<String,String> hashMap = Maps.newHashMap();
+        hashMap.put("nickName",nickName);
+        hashMap.put("picture",picture);
+
+        return hashMap;
     }
 
 }
